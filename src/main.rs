cbc189f07386da4258f045cd36b8776df7593fa1
Matt Chapman <matt@mattchapman.io>
2022-01-27 09:44:07 +1300

    Extract into module

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
-use std::io::{self, stdout};
-use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+mod editor;
+use editor::Editor;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Char(c) => {
-                    if c.is_control() {
-                        println!("{:?} \r", key)
-                    } else {
-                        println!("{:?} ({})\r", key, c)
-                    }
-                }
-                Key::Ctrl('q') => break,
-                _ => println!("{:?}\r", key),
-            },
-            Err(err) => die(err),
-        }
-    }
+    let editor = Editor {};
+    editor.run();
 }
 
-fn die(e: std::io::Error) {
-    panic!("{e}");
-}
