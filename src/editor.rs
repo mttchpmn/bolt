3e2fc3734cfe90097351448584a79d8335061182
Matt Chapman <matt@mattchapman.io>
2022-01-27 16:11:38 +1300

    Add terminal abstraction

--- src/editor.rs
+++ src/editor.rs
@@ -1,28 +1,29 @@
-use std::io::{self, stdout, Write};
-use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+use crate::Terminal;
+use termion::event::Key;
 
 pub struct Editor {
-    should_quit: bool
+    should_quit: bool,
+    terminal: Terminal,
 }
 
 impl Editor {
     pub fn default() -> Self {
         Self {
-            should_quit: false
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialise terminal"),
         }
     }
 
     pub fn run(&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
-
         loop {
             if let Err(err) = self.refresh_screen() {
                 die(&err);
             }
+
             if self.should_quit {
-                println!("Goodbye.\r");
                 break;
             }
+
             if let Err(err) = self.process_keypress() {
                 die(&err);
             }
@@ -30,25 +31,34 @@ impl Editor {
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        print!("{}{}", termion::clear::All, termion::cursor::Goto(1,1));
-        io::stdout().flush()
+        Terminal::cursor_hide();
+        Terminal::set_cursor_position(0, 0);
+
+        if self.should_quit {
+            Terminal::clear_screen();
+            println!("Goodbye.\r");
+        } else {
+            self.draw_rows();
+            Terminal::set_cursor_position(0, 0);
+        }
+        Terminal::cursor_show();
+        Terminal::flush()
+    }
+
+    fn draw_rows(&self) {
+        for _ in 0..self.terminal.size().height - 1 {
+            Terminal::clear_current_line();
+            println!("~\r");
+        }
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_key = read_key()?;
+        let pressed_key = Terminal::read_key()?;
 
         if let Key::Ctrl('q') = pressed_key {
             self.should_quit = true;
         }
         Ok(())
-    }
-}
-
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
     }
 }
 
